2a768fdc96ca56689d7fad18750ed13aa469a0f1
Al Duncanson <alDuncanson@proton.me>
2025-05-18 15:03:32 -0400

    Update flake.lock and flake.nix to use release-25.05 for nixpkgs and home-manager

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "Environment flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
