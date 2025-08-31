2d75b48b27c121de930eeebb46f7b742e5d043b9
Al Duncanson <alDuncanson@proton.me>
2025-08-31 11:04:36 -0400

    add nvf and zellij back

--- home.nix
+++ home.nix
@@ -98,5 +98,147 @@
         };
       };
     };
+<<<<<<< HEAD
+=======
+    nvf = {
+      enable = true;
+      settings = {
+        vim = {
+          assistant = {
+            copilot = {
+              enable = true;
+              cmp.enable = true;
+            };
+          };
+          enableLuaLoader = true;
+          autocomplete = {
+            enableSharedCmpSources = true;
+            blink-cmp.enable = true;
+          };
+          autopairs.nvim-autopairs.enable = true;
+          binds.whichKey.enable = true;
+          formatter = {
+            conform-nvim.enable = true;
+          };
+          git = {
+            enable = true;
+          };
+          hideSearchHighlight = true;
+          languages = {
+            enableExtraDiagnostics = true;
+            enableTreesitter = true;
+            enableFormat = true;
+            bash.enable = true;
+            css.enable = true;
+            go.enable = true;
+            html.enable = true;
+            lua.enable = true;
+            markdown.enable = true;
+            nix = {
+              enable = true;
+              format.type = "nixfmt";
+            };
+            python.enable = true;
+            tailwind = {
+              enable = true;
+              lsp.enable = true;
+            };
+            ts = {
+              enable = true;
+              format = {
+                enable = true;
+                type = "biome";
+              };
+            };
+            yaml.enable = true;
+            zig.enable = true;
+          };
+          lsp = {
+            enable = true;
+            formatOnSave = true;
+            lspconfig = {
+              enable = true;
+              sources = {
+                angular = ''require'lspconfig'.angularls.setup{}'';
+                harper = ''require('lspconfig').harper_ls.setup {}'';
+                dockercompose = ''
+                  require'lspconfig'.docker_compose_language_service.setup{
+                    filetypes = {
+                      'yaml',
+                      'yaml.docker-compose'
+                    }
+                  }
+                '';
+                docker = ''require'lspconfig'.dockerls.setup{}'';
+                biome = ''require'lspconfig'.biome.setup{}'';
+                svelte = ''require'lspconfig'.svelte.setup{}'';
+                html = ''
+                  --Enable (broadcasting) snippet capability for completion
+                  local capabilities = vim.lsp.protocol.make_client_capabilities()
+                  capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+                  require'lspconfig'.html.setup {
+                    capabilities = capabilities,
+                  }
+                '';
+              };
+            };
+            trouble.enable = true;
+          };
+          options = {
+            shiftwidth = 0;
+            tabstop = 2;
+            wrap = false;
+          };
+          statusline.lualine.enable = true;
+          telescope.enable = true;
+          terminal.toggleterm = {
+            enable = true;
+            lazygit.enable = true;
+          };
+          theme = {
+            enable = true;
+            name = "catppuccin";
+            style = "macchiato";
+            transparent = true;
+          };
+          ui = {
+            borders = {
+              enable = true;
+              globalStyle = "rounded";
+            };
+            breadcrumbs.enable = true;
+            fastaction.enable = true;
+            noice.enable = true;
+            colorizer.enable = true;
+          };
+          utility = {
+            preview.glow.enable = true;
+            yanky-nvim.enable = true;
+            yazi-nvim = {
+              enable = true;
+              setupOpts.open_for_directories = true;
+            };
+          };
+          clipboard = {
+            registers = [ "unnamedplus" ];
+          };
+        };
+      };
+    };
+    zellij = {
+      enable = true;
+      settings = {
+        theme = "catppuccin-macchiato";
+        ui = {
+          pane_frames = {
+            rounded_corners = true;
+            hide_session_name = true;
+          };
+        };
+        on_force_close = "quit";
+      };
+    };
+>>>>>>> parent of 2807b22 (Remove nvf, temporarily... can't fix errors and mostly using VS Code and cli's now)
   };
 }
