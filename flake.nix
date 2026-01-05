bcb592c7993cec4b0ae5b9de34f3296b954a7690
Al Duncanson <alDuncanson@proton.me>
2026-01-05 12:23:54 -0500

    use unstable nixpkgs
    switched to unstable to get the latest fixes (tree sitter, nvf, etc)

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "Environment flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
